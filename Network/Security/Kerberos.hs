8e0af73d124bd7e10f559de27dcefea99c64c17b
Ondrej Palkovsky <palkovsky.ondrej@gmail.com>
2016-11-09 23:56:10 +0100

    Added some comments.

--- Network/Security/Kerberos.hs
+++ Network/Security/Kerberos.hs
@@ -3,22 +3,26 @@
 {-# LANGUAGE MultiWayIf               #-}
 
 module Network.Security.Kerberos (
-  krb5Login
+    krb5Login
+  , krb5Resolve
+  , KrbException(..)
 ) where
 
-import           Control.Exception     (Exception, bracket, throwIO)
+import           Control.Exception     (Exception, bracket, throwIO, mask_)
+import           Control.Monad         (when)
 import qualified Data.ByteString.Char8 as BS
 import           Foreign
 import           Foreign.C.String
 import           Foreign.C.Types
-import Control.Monad (when)
+
+-- | Exception that can be thrown from calling kerberos functions
+data KrbException = KrbException Int String
+  deriving (Show)
+instance Exception KrbException
+
 
 data KerberosCTX
 data KerberosPrincipal
-
-data GSSException = KrbException Int String
-  deriving (Show)
-instance Exception GSSException
 
 type Context = Ptr KerberosCTX
 type Principal = ForeignPtr KerberosPrincipal
@@ -48,11 +52,32 @@ foreign import ccall unsafe "krb5.h &krb5_free_principal"
 krb5_parse_name :: Context -> BS.ByteString -> IO Principal
 krb5_parse_name ctx name =
   alloca $ \nprincipal ->
-    BS.useAsCString name $ \cname -> do
-      code <- _krb5_parse_name ctx cname nprincipal
-      ptr <- peek nprincipal
-      if | code == 0 -> newForeignPtrEnv _krb5_free_principal ctx ptr
-         | otherwise -> krb5_throw ctx code
+    BS.useAsCString name $ \cname ->
+      mask_ $ do
+        code <- _krb5_parse_name ctx cname nprincipal
+        if | code == 0 -> do
+                ptr <- peek nprincipal
+                newForeignPtrEnv _krb5_free_principal ctx ptr
+           | otherwise -> krb5_throw ctx code
+
+foreign import ccall unsafe "krb5.h krb5_unparse_name"
+  _krb5_unparse_name :: Context -> Ptr KerberosPrincipal -> Ptr CString -> IO CInt
+
+foreign import ccall unsafe "stdlib.h free"
+  _free :: Ptr a -> IO ()
+
+krb5_unparse_name :: Context -> Principal -> IO BS.ByteString
+krb5_unparse_name ctx principal =
+  withForeignPtr principal $ \ptrprincipal ->
+      alloca $ \nstring ->
+        mask_ $ do
+          code <- _krb5_unparse_name ctx ptrprincipal nstring
+          if | code == 0 -> do
+                  ctxt <- peek nstring
+                  result <- BS.packCString ctxt
+                  _free ctxt
+                  return result
+             | otherwise -> krb5_throw ctx code
 
 foreign import ccall unsafe "krb5.h krb5_get_error_message"
   _krb5_get_error_message :: Ptr KerberosCTX -> CInt -> IO CString
@@ -76,8 +101,17 @@ krb5_login ctx principal password = do
           _krb5_login ctx ptrprincipal cpass
   when (code /= 0) $ krb5_throw ctx code
 
+-- | Try to login with servicename and password
+-- If logging fails, exception is raised
 krb5Login :: BS.ByteString -> BS.ByteString -> IO ()
 krb5Login svcname password =
   withKrbContext $ \ctx -> do
       principal <- krb5_parse_name ctx svcname
       krb5_login ctx principal password
+
+-- | Call 'krb5_unparse . krb5_parse' and return result
+krb5Resolve :: BS.ByteString -> IO BS.ByteString
+krb5Resolve svcname =
+  withKrbContext $ \ctx -> do
+      principal <- krb5_parse_name ctx svcname
+      krb5_unparse_name ctx principal
