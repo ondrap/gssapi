cd9831251bdfd62e537491f4bf521fe27ade3814
Ondrej Palkovsky <palkovsky.ondrej@gmail.com>
2016-11-10 15:40:16 +0100

    Better code with allocations.

--- src/Network/Security/GssApi.hs
+++ src/Network/Security/GssApi.hs
@@ -1,14 +1,17 @@
 {-# LANGUAGE CApiFFI                    #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiWayIf                 #-}
+{-# LANGUAGE OverloadedStrings                 #-}
 
 module Network.Security.GssApi where
 
-import           Control.Exception         (Exception, bracket, mask_, throwIO)
-import           Control.Monad             (void)
-import qualified Data.ByteString.Char8     as BS
-import           Foreign                   (Ptr, Storable, alloca, nullPtr,
-                                            peek, poke)
+import           Control.Exception            (Exception, mask_, throwIO, finally)
+import           Control.Monad                (void)
+import           Control.Monad.IO.Class       (MonadIO, liftIO)
+import           Control.Monad.Trans.Resource (ResourceT, allocate)
+import qualified Data.ByteString.Char8        as BS
+import           Foreign                      (Ptr, Storable, alloca, nullPtr,
+                                               peek, poke)
 import           Foreign.C.Types
 
 import           Network.Security.GssTypes
@@ -43,17 +46,10 @@ foreign import capi "gssapi/gssapi.h GSS_ERROR" _gssError :: CUInt -> CUInt
 gssError :: CUInt -> Bool
 gssError major = _gssError major /= 0
 
-withBufferDesc :: BS.ByteString -> (Ptr BufferDesc -> IO a) -> IO a
-withBufferDesc str code =
-  BS.useAsCStringLen str $ \(cstr, len) ->
-    alloca $ \bdesc -> do
-      poke bdesc (BufferDesc len cstr)
-      code bdesc
-
-foreign import ccall "gssapi/gssapi.h gss_import_name"
+foreign import ccall unsafe "gssapi/gssapi.h gss_import_name"
   _gss_import_name :: Ptr CUInt -> Ptr BufferDesc -> GssOID -> Ptr GssNameT -> IO CUInt
 
-foreign import ccall "gssapi/gssapi.h gss_release_name"
+foreign import ccall unsafe "gssapi/gssapi.h gss_release_name"
   _gss_release_name :: Ptr CUInt -> GssNameT -> IO CUInt
 
 gssReleaseName :: GssNameT -> IO ()
@@ -61,40 +57,37 @@ gssReleaseName name =
   alloca $ \minor ->
     void $ _gss_release_name minor name
 
-gssImportName :: BS.ByteString -> IO GssNameT
-gssImportName svc =
-  withBufferDesc svc $ \bptr ->
-    alloca $ \minor ->
-      alloca $ \gssname -> do
-        major <- _gss_import_name minor bptr gssKrb5NtPrincipalName gssname
-        whenGssOk major minor $ peek gssname
+gssImportName :: BS.ByteString -> ResourceT IO GssNameT
+gssImportName svc = snd <$> allocate doimport gssReleaseName
+  where
+    doimport =
+      withBufferDesc svc $ \bptr ->
+        alloca $ \minor ->
+          alloca $ \gssname -> do
+            major <- _gss_import_name minor bptr gssKrb5NtPrincipalName gssname
+            whenGssOk major minor $ peek gssname
 
-withGssName :: BS.ByteString -> (GssNameT -> IO a) -> IO a
-withGssName name = bracket (gssImportName name) gssReleaseName
-
-foreign import ccall "gssapi/gssapi.h gss_display_name"
+foreign import ccall unsafe "gssapi/gssapi.h gss_display_name"
   _gss_display_name :: Ptr CUInt -> GssNameT -> Ptr BufferDesc -> Ptr (Ptr GssOID) -> IO CUInt
 
-foreign import ccall "gssapi/gssapi.h gss_release_buffer"
+foreign import ccall unsafe "gssapi/gssapi.h gss_release_buffer"
   _gss_release_buffer :: Ptr CUInt -> Ptr BufferDesc -> IO CUInt
 
-peekBufferAndFree :: Ptr BufferDesc -> IO BS.ByteString
-peekBufferAndFree bdesc = do
+peekBuffer :: Ptr BufferDesc -> IO BS.ByteString
+peekBuffer bdesc = do
   (BufferDesc len ptr) <- peek bdesc
-  res <- BS.packCStringLen (ptr, len)
-  alloca $ \minor -> void $ _gss_release_buffer minor bdesc
-  return res
+  BS.packCStringLen (ptr, len)
 
-gssDisplayName :: GssNameT -> IO BS.ByteString
+gssDisplayName :: MonadIO m => GssNameT -> m BS.ByteString
 gssDisplayName gname =
-  mask_ $
-    alloca $ \bdesc ->
+  liftIO $ mask_ $
+    withBufferDesc "" $ \bdesc ->
       alloca $ \minor -> do
         poke bdesc (BufferDesc 0 nullPtr)
         major <- _gss_display_name minor gname bdesc nullPtr
-        whenGssOk major minor $ peekBufferAndFree bdesc
+        whenGssOk major minor $ peekBuffer bdesc
 
-foreign import ccall "gssapi/gssapi.h gss_display_status"
+foreign import ccall unsafe "gssapi/gssapi.h gss_display_status"
   _gss_display_status :: Ptr CUInt -> CUInt -> CUInt -> GssOID -> Ptr CUInt -> Ptr BufferDesc -> IO CUInt
 
 whenGssOk :: CUInt -> Ptr CUInt -> IO a -> IO a
@@ -111,20 +104,48 @@ gssDisplayStatus rstatus =
   alloca $ \minor ->
       alloca $ \msgctx -> do
           poke msgctx 0
-          alloca $ \bdesc -> do
+          withBufferDesc "" $ \bdesc -> do
               poke bdesc (BufferDesc 0 nullPtr)
               major <- _gss_display_status minor rstatus gssCMechCode gssCNoOid msgctx bdesc
-              whenGssOk major minor $ peekBufferAndFree bdesc
+              whenGssOk major minor $ peekBuffer bdesc
 
-foreign import ccall "gssapi/gssapi.h gss_acquire_cred"
+foreign import ccall safe "gssapi/gssapi.h gss_acquire_cred"
   _gss_acquire_cred :: Ptr CUInt -> GssNameT -> CUInt -> GssOIDSet -> GssCredUsageT -> Ptr GssCredIdT -> Ptr GssOIDSet -> Ptr CUInt -> IO CUInt
 
-gssAcquireCred :: GssNameT -> IO GssCredIdT
+gssAcquireCred :: GssNameT -> ResourceT IO GssCredIdT
 gssAcquireCred name =
-  alloca $ \minor ->
-    alloca $ \credid -> do
-      major <- _gss_acquire_cred minor name gscCIndefinite gssCNoOidSet gssCAccept credid nullPtr nullPtr
-      whenGssOk major minor $ peek credid
+    snd <$> allocate doalloc gssReleaseCred
+  where
+    doalloc =
+      liftIO $ alloca $ \minor ->
+        alloca $ \credid -> do
+          major <- _gss_acquire_cred minor name gscCIndefinite gssCNoOidSet gssCAccept credid nullPtr nullPtr
+          whenGssOk major minor $ peek credid
 
--- foreign import ccall "gssapi/gssapi.h gss_accept_sec_context"
---   _gss_accept_sec_context :: Ptr CUInt -> Ptr GssCtxIdT -> GssCredIdT -> Ptr BufferDesc -> GssChannelBindingsT ->
+foreign import ccall unsafe "gssapi/gssapi.h gss_release_cred"
+  _gss_release_cred :: Ptr CUInt -> GssCredIdT -> IO CUInt
+
+gssReleaseCred :: GssCredIdT -> IO ()
+gssReleaseCred name = alloca $ \minor -> void $ _gss_release_cred minor name
+
+gssReleaseBuffer :: Ptr BufferDesc -> IO ()
+gssReleaseBuffer bdesc = void $ alloca $ \minor -> _gss_release_buffer minor bdesc
+
+-- foreign import ccall safe "gssapi/gssapi.h gss_accept_sec_context"
+--   _gss_accept_sec_context :: Ptr CUInt -> Ptr GssCtxIdT -> GssCredIdT -> Ptr BufferDesc -> GssChannelBindingsT
+--                               -> Ptr GssNameT -> Ptr GssOID -> Ptr BufferDesc -> Ptr CUInt -> Ptr CUInt
+--                               -> Ptr GssCredIdT -> IO CUInt
+--
+
+
+---
+withBufferDesc :: BS.ByteString -> (Ptr BufferDesc -> IO a) -> IO a
+withBufferDesc "" code =
+  alloca $ \bdesc -> do
+      poke bdesc (BufferDesc 0 nullPtr)
+      code bdesc `finally` gssReleaseBuffer bdesc
+withBufferDesc str code =
+  BS.useAsCStringLen str $ \(cstr, len) ->
+    alloca $ \bdesc -> do
+      poke bdesc (BufferDesc len cstr)
+      code bdesc
